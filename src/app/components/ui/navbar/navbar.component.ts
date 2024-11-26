9e952687323d9013d96ca82fd497a1c1af91a619
LipeSou <felipesoutomdt@gmail.com>
2024-11-26 00:35:57 -0300

    improve navbar

--- src/app/components/ui/navbar/navbar.component.ts
+++ src/app/components/ui/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { phosphorPopcornBold } from '@ng-icons/phosphor-icons/bold';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+providers: [provideIcons({ phosphorPopcornBold })],
+
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {}
