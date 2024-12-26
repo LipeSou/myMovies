464c810b3cf7af8854da1dfa8430f49d1f97a6d7
LipeSou <felipesoutomdt@gmail.com>
2024-12-26 17:21:31 -0300

    feat:  initialize login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MovieDetailsContent } from './components/movieDetails/movieDetailsContent/movieDetailsContent';
 import { ContentComponent } from './components/home/content/content.component';
 import { SearchContentComponent } from './components/search/content/searchContent.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: ContentComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'search', component: SearchContentComponent},
   { path: 'detalhes-filme/:type/:id', component: MovieDetailsContent}
 ];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { phosphorPopcorn } from '@ng-icons/phosphor-icons/regular';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [InputTextModule, CommonModule, ReactiveFormsModule, NgIcon],
+  providers: [provideIcons({ phosphorPopcorn })],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  constructor(private router: Router) {}
+
+  toHomePage() {
+    this.router.navigate(['/'])
+  }
+
+  // input de busca e debounce 
+  searchLogin = new FormGroup({
+    name: new FormControl('') ,
+    password: new FormControl('') 
+  });
+
+  onLogin() {
+    // this.searchLogin.get('name')?.valueChanges
+    //   .pipe()
+    //   .subscribe((query: string | null) => {
+    //     if ((query as string)?.trim().length > 0) {
+    //       this.onSearch(query as string);
+    //     } else {
+    //       // Se o usuário apagar o texto, limpa os resultados da busca 
+    //       this.searchMovies = [];
+    //     }
+    //   });
+    console.log("login feito!")
+    this.toHomePage()
+  }
+}
